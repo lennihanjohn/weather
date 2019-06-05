4894a2c0f84e24f60ccf9953f7968adb58200168
linhan <unitimothy@gmail.com>
2019-06-05 10:10:52 -0400

    WIP: set up weather forcast attributes

--- app/models/weather_forcast.rb
+++ app/models/weather_forcast.rb
@@ -25,14 +25,12 @@ class WeatherForcast < ApplicationRecord
     accepts_nested_attributes_for :weather_description, allow_destroy: true
     accepts_nested_attributes_for :city, allow_destroy: true
     accepts_nested_attributes_for :country, allow_destroy: true
-
-        # "weather"=>[{"id"=>804, "main"=>"Clouds", "description"=>"overcast clouds", "icon"=>"04n"}], "base"=>"stations", "main"=>{"temp"=>283.04, "pressure"=>1011, "humidity"=>93, "temp_min"=>282.59, "temp_max"=>283.71}, "visibility"=>24140, "wind"=>{"speed"=>2.1, "deg"=>210}, "clouds"=>{"all"=>90}, "dt"=>1559705672, "sys"=>{"type"=>1, "id"=>872, "message"=>0.0059, "country"=>"CA", "sunrise"=>1559639791, "sunset"=>1559695554}, "timezone"=>-14400, "id"=>6094817, "name"=>"Ottawa", "cod"=>200}n  
        
     def store_data(params)
         weather_description << weather_description_params(params)
         city << city_params(params)
         country << country_params(params)
-        weather_forcast << {}
+        weather_forcast << weather_forcast_params(params)
     end
 
     private
@@ -40,13 +38,13 @@ class WeatherForcast < ApplicationRecord
      # return weather attributes
      def weather_forcast_params(params)
         return {
-            temperature: params['name'], 
-            min_temperature: params['coord']['lat'], 
-            max_temperature: params['coord']['lat'], 
-            humidity: params['coord']['lat'], 
-            pressure: params['coord']['lat'], 
-            sunrise: params['coord']['lat'], 
-            sunset: params['coord']['lat']
+            temperature: params['main']['temp'], 
+            min_temperature: params['main']['temp_min'], 
+            max_temperature: params['main']['temp_max'], 
+            humidity: params['main']['humidity'], 
+            pressure: params['main']['pressure'], 
+            sunrise: params['sys']['sunrise'], 
+            sunset: params['sys']['sunset']
         }
     end
 
