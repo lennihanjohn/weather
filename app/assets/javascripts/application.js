2d3c26d8784fdfd6fbf3c4b78593429a9a44383e
linhan <unitimothy@gmail.com>
2019-06-06 10:10:44 -0400

    fixed minor bug and disable the button if clicked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-$('.btn').on('click', function() {
-    var $this = $(this);
-  $this.button('loading');
-    setTimeout(function() {
-       $this.button('reset');
-   }, 1000);
-});
